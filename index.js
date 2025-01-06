ce108d7ad722b4cfe5692a20a37dd7cfa9f55f9b
YukkieWQR <pawwne27@gmail.com>
2025-01-06 17:59:02 +0300

    fc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+
+/*
+const is variable that will never change
+yooooooooooooooooooooooooooooooooooooooooooooo
+
+ */
+const Lol4 = 3.5
+Lol = "lol yo var";
+
+var Yo = "Boom"
+
+var Lol = 1
+var Lol2 = 1.12
+var Lol3 = 2.12
+Lol2 -= 1.10
+console.log(Lol2);
+
+
+
