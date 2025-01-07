bae233e10b92bb9b12c2b063e9e301a8c68f0370
YukkieWQR <pawwne27@gmail.com>
2025-01-07 18:10:43 +0300

    fc

--- index.js
+++ index.js
@@ -4,16 +4,53 @@ const is variable that will never change
 yooooooooooooooooooooooooooooooooooooooooooooo
 
  */
-const Lol4 = 3.5
-Lol = "lol yo var";
+/*
 
-var Yo = "Boom"
 
-var Lol = 1
-var Lol2 = 1.12
-var Lol3 = 2.12
-Lol2 -= 1.10
-console.log(Lol2);
+
+function variables(){
+
+    const Lol4 = 3.5
+    Lol = "lol yo var";
+
+    var Yo = "Boom"
+
+    var Lol = 1
+    var Lol2 = 1.12
+    var Lol3 = 2.12
+    Lol2 -= 1.10
+    console.log(Lol2);
+
+}
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
